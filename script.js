bf31e8c6dc2ade6ca5796ec024e385437b332a4a
Samta <108336676+Smta028@users.noreply.github.com>
2023-04-02 22:17:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
+	var newStr = str.split(" ");
+	var resultStr = newStr.join("");
+	 resultStr = resultStr.toLowerCase();
 
+	for(var i =0;i<resultStr.length;i++){
+		if(resultStr[i] !==  resultStr[resultStr.length - 1 - i]){
+			return false;
+		}
+	}
+	return true;
+	
 }
 module.exports = palindrome
